3ad0b2559045f7167689e7080052da88a9a47eed
Brendan Anderson <SirMorek@gmail.com>
2018-07-05 16:42:06 -0600

    Webservice: Add file level caching for basic persistence
    This gets the job done with carrying forward the generated teams
    until they are requested to be regenerated. That will be as simple
    as nuking the file and re-running the matchmaker with new params.

--- web-service/app.py
+++ web-service/app.py
@@ -4,15 +4,48 @@ from os import path
 from flask import Flask
 
 from matchmaker.main import matchmake
+from matchmaker.team import Team
+from matchmaker.player import HockeyPlayer
 
 APP = Flask(__name__)
 PLAYER_FILE = path.join(path.dirname(__file__), '..', 'players.json')
+PERSISTENCE_FILE = path.join(path.dirname(__file__), 'persistence.json')
 
 
 @APP.route("/")
 def root():
     with open(PLAYER_FILE) as player_file:
         player_json = json.loads(player_file.read())
-        teams, remainder = matchmake(player_json['players'], 6)
+        teams, players = matchmake(player_json['players'], 6)
 
-    return "Teams: %s\nRemainder: %s" % (teams, remainder)
+    with open(PERSISTENCE_FILE, "w") as outfile:
+        outfile.write(
+            json.dumps({
+                "teams": Team.bulk_to_json(teams),
+                "players": [player.to_json() for player in players]
+            }))
+
+    return "Teams: %s, Free players: %s" % (teams, players)
+
+
+@APP.route("/teams", methods=["GET", "POST"])
+def view():
+    try:
+        with open(PERSISTENCE_FILE) as cache:
+            cache_results = json.loads(cache.read())
+            players = cache_results['players']
+            teams = cache_results['teams']
+        players = [
+            HockeyPlayer.from_json(player_json) for player_json in players
+        ]
+        teams = [Team.from_json(team_json) for team_json in teams]
+    except FileNotFoundError as e:
+        with open(PLAYER_FILE) as player_file:
+            _player_json = json.loads(player_file.read())
+            players = [
+                HockeyPlayer.from_json(player_json)
+                for player_json in _player_json['players']
+            ]
+            teams = []
+    return "Teams: {teams}, Free players: {players},".format(
+        players=players, teams=teams)
