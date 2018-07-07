7834c5a7c92f94a199fed9b52d237ee842b9b366
Brendan Anderson <SirMorek@gmail.com>
2018-07-06 21:42:42 -0600

    Matchmaker: Switch main algorithm over to tree search

--- matchmaker/matchmaker/main.py
+++ matchmaker/matchmaker/main.py
@@ -3,7 +3,7 @@ The matchmaker service is responsible for taking the set of players and
 producing a list of teams, plus the "wait list" of the remaining players who
 could not be fit on a team.
 '''
-from matchmaker.assign import round_robin_assign_players
+from matchmaker.assign import tree_search_assign_players
 from matchmaker.player import HockeyPlayer
 from matchmaker.team import Team
 
@@ -11,5 +11,5 @@ from matchmaker.team import Team
 def matchmake(player_list, num_teams):
     players = [HockeyPlayer.from_json(player) for player in player_list]
     teams = [Team() for i in range(num_teams)]
-    remainder = round_robin_assign_players(players, teams)
+    remainder = tree_search_assign_players(players, teams)
     return teams, remainder
